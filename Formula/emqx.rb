8a52d1c9d214b6f9ee0f6e77b5108f83a5d57674
zhanghongtong <zhanghongtong@foxmail.com>
2020-02-21 11:06:17 +0000

    update v4.0.3

--- Formula/emqx.rb
+++ Formula/emqx.rb
@@ -1,8 +1,8 @@
 class Emqx < Formula
   homepage "https://emqx.io"
-  url "https://repos.emqx.io/emqx-ce/homebrew/emqx-homebrew-v4.0.2.zip"
-  sha256 "d310fe371ebbfe5e7815de5d47cbc8b339bf98cf05c6a81a5a0a1f1ec8e1ead3"
-  version "4.0.2"
+  url "https://repos.emqx.io/emqx-ce/homebrew/emqx-homebrew-v4.0.3.zip"
+  sha256 "e03ae7e638b9c1780ed905460a4cbbf44f34bbd87d4b2856695ce77de9d35e3a"
+  version "4.0.3"
 
   depends_on "openssl"
 
